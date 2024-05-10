bcb5d57c7b8bc31cb9a2f0b357fa5d1039d6be54
vaibhav saxena <121886758+SaxenaVaibhav80@users.noreply.github.com>
2024-05-10 06:01:39 +0530

    learning various attributes that we use in form to change or update the data using formik.handleChange, formik.values. so on.....

--- formik/src/components/Form.jsx
+++ formik/src/components/Form.jsx
@@ -1,22 +1,29 @@
 
-
 import {useFormik} from 'formik'
+
 
 function Form()
 {
-    const formik=useFormik({})
-    console.log(formik)
+    const formik=useFormik({
+
+        initialValues:{
+            name:'',
+            email:'',
+            channel :''
+        }
+    })
+    console.log(formik.values)// you can access those object passed in the useFormik by writing formik.values
     return(
         <>
            <form>
               <label htmlFor="myName">Name</label>
-              <input name="name" id="myName" type="text"/>
+              <input name="name" id="myName" type="text" onChange={formik.handleChange} value={formik.values.name}/>
 
               <label htmlFor="myEmail">E-mail</label>
-              <input name="email" id="myEmail" type="email"/>
+              <input name="email" id="myEmail" type="email" onChange={formik.handleChange} value={formik.values.email}/>
 
               <label htmlFor="myCh">Channel</label>
-              <input name="channel" id="myCh" type="text"/>
+              <input name="channel" id="myCh" type="text"onChange={formik.handleChange} value={formik.values.channel}/>
 
               <button>Submit</button>
            </form>
@@ -29,9 +36,12 @@ export default Form
 
 
 // Formik will take an object with some value as gievn below----->
-
 // useFormik({ validateOnChange, validateOnBlur, validateOnMount, isInitialValid, enableReinitialize, onSubmit, ...rest }: FormikConfig<FormikValues>)
 
 // useFormik with empty object return this ---->
-
 // {values: undefined, errors: {…}, touched: {…}, status: undefined, isSubmitting: false, …}
+
+// although we can ahndle the data or manage the data using states but this formik made it easy to handle
+
+// Note--->
+// In Formik, the keys used in the initialValues object correspond to the name attribute of your form inputs
